0196d5e9c3184cda724c1ebde8ca0e6454e5dec5
Brad London and Chris Young <london.brad@gmail.com>
2016-03-15 16:51:38 -0700

    change append

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     var zip = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + zip + ',us&appid=' + apiKey).then(function(response) {
-      $('.showWeather').text("The temperature in " + response.name + " is " + response.main.temp + " Kelvin and " + convertTempToC(response.main.temp) + " in Celsius and " + convertTempToF(response.main.temp) + " in Fahrenheit" );
+      $('.showWeather').append("<h3>The temperature in " + response.name + " is:</h3><ul><li>" + response.main.temp + "&deg; Kelvin</li><li>" + convertTempToC(response.main.temp) + "&deg; Celsius</li><li>" + convertTempToF(response.main.temp) + "&deg; Fahrenheit");
       console.log(response);
     }).fail(function(error) {
       $('.showWeather').text(error.responseJSON.message);
